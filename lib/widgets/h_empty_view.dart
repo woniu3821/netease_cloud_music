53bd6c86c18e973459885abe1241a8eaf9f078fa
taocui <taocui@wisedu.com>
2019-12-11 15:33:26 +0800

    day 1

---
+++ lib/widgets/h_empty_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HEmptyView extends StatelessWidget {
+  final double width;
+  HEmptyView(this.width);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: width,
+    );
+  }
+}
